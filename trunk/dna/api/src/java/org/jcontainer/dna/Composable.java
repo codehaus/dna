39def7daffb639dbf458b1a9c2249d432794a260
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-07-25 11:34:35 +0000

    Add initial cut of framework API

---
+++ trunk/dna/api/src/java/org/jcontainer/dna/Composable.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.dna;
+
+/**
+ *
+ * @version $Revision: 1.1 $ $Date: 2003-07-25 11:34:35 $
+ */
+public interface Composable
+{
+    void compose( ResourceLocator locator )
+        throws MissingResourceException;
+}
---
+++ trunk/dna/api/src/java/org/jcontainer/dna/ConfigurationException.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.dna;
+
+/**
+ *
+ * @version $Revision: 1.1 $ $Date: 2003-07-25 11:34:35 $
+ */
+public class ConfigurationException
+    extends Exception
+{
+    private final Throwable m_cause;
+    private final String m_location;
+
+    public ConfigurationException( final String message,
+                                   final String location )
+    {
+        this( message, null, location );
+    }
+
+    public ConfigurationException( final String message,
+                                   final Throwable cause,
+                                   final String location )
+    {
+        super( message );
+        m_cause = cause;
+        m_location = location;
+    }
+
+    public String getLocation()
+    {
+        return m_location;
+    }
+
+    public Throwable getCause()
+    {
+        return m_cause;
+    }
+}
