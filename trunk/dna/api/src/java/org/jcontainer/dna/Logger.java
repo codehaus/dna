c34c356ba0caa657d91727a744c43769f1efaf73
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-07-26 04:02:13 +0000

    Notice --> info

--- trunk/dna/api/src/java/org/jcontainer/dna/Logger.java
+++ trunk/dna/api/src/java/org/jcontainer/dna/Logger.java
@@ -9,7 +9,7 @@ package org.jcontainer.dna;
 
 /**
  *
- * @version $Revision: 1.2 $ $Date: 2003-07-26 03:59:02 $
+ * @version $Revision: 1.3 $ $Date: 2003-07-26 04:02:13 $
  */
 public interface Logger
 {
@@ -25,11 +25,11 @@ public interface Logger
 
     boolean isDebugEnabled();
 
-    void notice( String message );
+    void info( String message );
 
-    void notice( String message, Throwable throwable );
+    void info( String message, Throwable throwable );
 
-    boolean isNoticeEnabled();
+    boolean isInfoEnabled();
 
     void warn( String message );
 
