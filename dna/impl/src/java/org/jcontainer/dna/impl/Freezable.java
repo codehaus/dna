949b761974bb5dce3a32058590e3fd48dfce52ba
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-07-27 02:03:46 +0000

    Add interface to mark objects that can become "frozen" or read-only

---
+++ dna/impl/src/java/org/jcontainer/dna/impl/Freezable.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.dna.impl;
+
+/**
+ *
+ * @version $Revision: 1.1 $ $Date: 2003-07-27 02:03:46 $
+ */
+public interface Freezable
+{
+    void makeReadOnly();
+}
