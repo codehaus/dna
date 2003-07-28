38063b7a59ddc96e32f4b1feda9c3272c21cf93d
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-07-28 08:21:19 +0000

    Add ConfigurationUtil to make it easy to build Configuration objects from XML InputSources or from Element objects.
    Added Configutation -> Element util method aswell

---
+++ trunk/dna/impl/src/java/org/jcontainer/dna/impl/ConfigurationUtil.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.dna.impl;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.jcontainer.dna.Configuration;
+import org.w3c.dom.CharacterData;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.InputSource;
+
+/**
+ *
+ * @version $Revision: 1.1 $ $Date: 2003-07-28 08:21:19 $
+ */
+public class ConfigurationUtil
+{
+    public static Configuration buildFromXML( final InputSource input )
+        throws Exception
+    {
+        final SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        saxParserFactory.setNamespaceAware( false );
+        final SAXParser saxParser = saxParserFactory.newSAXParser();
+        final SAXConfigurationHandler handler = new SAXConfigurationHandler();
+        saxParser.parse( input, handler );
+        return handler.getConfiguration();
+    }
+
+    public static Configuration toConfiguration( final Element element )
+    {
+        final DefaultConfiguration configuration =
+            new DefaultConfiguration( element.getNodeName(), "dom-created" );
+        final NamedNodeMap attributes = element.getAttributes();
+        final int length = attributes.getLength();
+        for( int i = 0; i < length; i++ )
+        {
+            final Node node = attributes.item( i );
+            final String name = node.getNodeName();
+            final String value = node.getNodeValue();
+            configuration.setAttribute( name, value );
+        }
+
+        String content = null;
+        final NodeList nodes = element.getChildNodes();
+        final int count = nodes.getLength();
+        for( int i = 0; i < count; i++ )
+        {
+            final Node node = nodes.item( i );
+            if( node instanceof Element )
+            {
+                final Configuration child = toConfiguration( (Element)node );
+                configuration.addChild( child );
+            }
+            else if( node instanceof CharacterData )
+            {
+                final CharacterData data = (CharacterData)node;
+                content += data.getData();
+            }
+        }
+
+        if( null != content )
+        {
+            configuration.setValue( content );
+        }
+
+        return configuration;
+    }
+
+    public static Element toElement( final org.apache.avalon.framework.configuration.Configuration configuration )
+    {
+        try
+        {
+            final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder builder = factory.newDocumentBuilder();
+            final Document document = builder.newDocument();
+
+            return createElement( document, configuration );
+        }
+        catch( final Exception ce )
+        {
+            throw new IllegalStateException( ce.toString() );
+        }
+    }
+
+    private static Element createElement( final Document document,
+                                          final org.apache.avalon.framework.configuration.Configuration configuration )
+    {
+        final Element element = document.createElement( configuration.getName() );
+
+        final String content = configuration.getValue( null );
+        if( null != content )
+        {
+            final Text child = document.createTextNode( content );
+            element.appendChild( child );
+        }
+
+        final String[] names = configuration.getAttributeNames();
+        for( int i = 0; i < names.length; i++ )
+        {
+            final String name = names[ i ];
+            final String value = configuration.getAttribute( name, null );
+            element.setAttribute( name, value );
+        }
+        final org.apache.avalon.framework.configuration.Configuration[] children = configuration.getChildren();
+        for( int i = 0; i < children.length; i++ )
+        {
+            final Element child = createElement( document, children[ i ] );
+            element.appendChild( child );
+        }
+        return element;
+    }
+}
