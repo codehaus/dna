7b1d1f23484bfc96dad9d484f0c5059332ea7cf9
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-05 06:01:51 +0000

    Javadocs

--- trunk/dna/api/src/java/org/jcontainer/dna/Parameters.java
+++ trunk/dna/api/src/java/org/jcontainer/dna/Parameters.java
@@ -8,39 +8,145 @@
 package org.jcontainer.dna;
 
 /**
+ * Parameters present flat configuration data. Contained
+ * in the Parameters object is a set of name-value pairs.
  *
- * @version $Revision: 1.5 $ $Date: 2003-08-07 04:30:06 $
+ * @version $Revision: 1.6 $ $Date: 2003-09-05 05:58:23 $
  */
 public interface Parameters
 {
-    String[] getParameterNames();
+   /**
+    * Return the names of all the parameters.
+    *
+    * @return the names of all the parameters.
+    */
+   String[] getParameterNames();
 
-    boolean isParameter( String name );
+   /**
+    * Return true of parameter with specified name exists.
+    *
+    * @param name the name
+    * @return true of parameter with specified name exists.
+    */
+   boolean isParameter( String name );
 
-    String getParameter( String name )
-        throws ParameterException;
+   /**
+    * Return value of parameter with specified name.
+    *
+    * @param name the name
+    * @return the value
+    * @throws ParameterException if unable to locate parameter
+    */
+   String getParameter( String name )
+      throws ParameterException;
 
-    String getParameter( String name, String defaultValue );
+   /**
+    * Return value of parameter with specified name.
+    *
+    * @param name the name
+    * @param defaultValue the defaultValue if specified parameter
+    *        does not exist
+    * @return the value
+    */
+   String getParameter( String name, String defaultValue );
 
-    int getParameterAsInteger( String name )
-        throws ParameterException;
+   /**
+    * Return value of parameter with specified name as an integer.
+    *
+    * @param name the name
+    * @return the value
+    * @throws ParameterException if unable to locate parameter
+    *         or parameter can not be converted to correct type
+    */
+   int getParameterAsInteger( String name )
+      throws ParameterException;
 
-    int getParameterAsInteger( String name, int defaultValue );
+   /**
+    * Return value of parameter with specified name as an integer.
+    *
+    * @param name the name
+    * @param defaultValue the defaultValue if specified parameter
+    *        does not exist or parameter can not be converted to
+    *        the correct type
+    * @return the value
+    */
+   int getParameterAsInteger( String name, int defaultValue );
 
-    long getParameterAsLong( String name )
-        throws ParameterException;
+   /**
+    * Return value of parameter with specified name as a long.
+    *
+    * @param name the name
+    * @return the value
+    * @throws ParameterException if unable to locate parameter
+    *         or parameter can not be converted to correct type
+    */
+   long getParameterAsLong( String name )
+      throws ParameterException;
 
-    long getParameterAsLong( String name, long defaultValue );
+   /**
+    * Return value of parameter with specified name as a long.
+    *
+    * @param name the name
+    * @param defaultValue the defaultValue if specified parameter
+    *        does not exist or parameter can not be converted to
+    *        the correct type
+    * @return the value
+    */
+   long getParameterAsLong( String name, long defaultValue );
 
-    boolean getParameterAsBoolean( String name )
-        throws ParameterException;
+   /**
+    * Return value of parameter with specified name as a boolean.
+    *
+    * @param name the name
+    * @return the value
+    * @throws ParameterException if unable to locate parameter
+    *         or parameter can not be converted to correct type
+    */
+   boolean getParameterAsBoolean( String name )
+      throws ParameterException;
 
-    boolean getParameterAsBoolean( String name, boolean defaultValue );
+   /**
+    * Return value of parameter with specified name as a boolean.
+    *
+    * @param name the name
+    * @param defaultValue the defaultValue if specified parameter
+    *        does not exist or parameter can not be converted to
+    *        the correct type
+    * @return the value
+    */
+   boolean getParameterAsBoolean( String name, boolean defaultValue );
 
-    float getParameterAsFloat( String name )
-        throws ParameterException;
+   /**
+    * Return value of parameter with specified name as a float.
+    *
+    * @param name the name
+    * @return the value
+    * @throws ParameterException if unable to locate parameter
+    *         or parameter can not be converted to correct type
+    */
+   float getParameterAsFloat( String name )
+      throws ParameterException;
 
-    float getParameterAsFloat( String name, float defaultValue );
+   /**
+    * Return value of parameter with specified name as a float.
+    *
+    * @param name the name
+    * @param defaultValue the defaultValue if specified parameter
+    *        does not exist or parameter can not be converted to
+    *        the correct type
+    * @return the value
+    */
+   float getParameterAsFloat( String name, float defaultValue );
 
-    Parameters getChildParameters( String prefix );
+   /**
+    * Return a Parameters object that represents a
+    * subset of parameters with specified prefix + the '.'
+    * character. if the prefix was "foo" then the parameter
+    * "foo.baz" would be included in child Parameters object
+    * using the key "baz".
+    *
+    * @param prefix the prefix
+    * @return the parameters object
+    */
+   Parameters getChildParameters( String prefix );
 }
--- trunk/dna/api/src/java/org/jcontainer/dna/ReleaseUtil.java
+++ trunk/dna/api/src/java/org/jcontainer/dna/ReleaseUtil.java
@@ -8,21 +8,43 @@
 package org.jcontainer.dna;
 
 /**
+ * Utility class to signal to the container that
+ * a resource is no longer going to be used by
+ * the component.
  *
- * @version $Revision: 1.1 $ $Date: 2003-07-28 09:06:58 $
+ * @version $Revision: 1.2 $ $Date: 2003-09-05 06:01:51 $
  */
 public class ReleaseUtil
 {
-    public interface Releaseable
-    {
-        void release();
-    }
+   /**
+    * Utility interface used to mark resources that
+    * can be released. Developers should never directly
+    * reference this class and never make a component
+    * implement this interface. Instead the container
+    * will choose to have the proxys of component implement
+    * the interface.
+    */
+   public interface Releaseable
+   {
+      /**
+       * Indicate to the container that component no
+       * longer needs resources.
+       */
+      void release();
+   }
 
-    public static void release( final Object object )
-    {
-        if( object instanceof Releaseable )
-        {
-            ((Releaseable)object).release();
-        }
-    }
+   /**
+    * The component invokes this method to indicate to
+    * the container that it no longer needs specified
+    * resource.
+    *
+    * @param object the resource
+    */
+   public static void release( final Object object )
+   {
+      if ( object instanceof Releaseable )
+      {
+         ( (Releaseable) object ).release();
+      }
+   }
 }
