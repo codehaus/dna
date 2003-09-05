01606738a10018d1c4a133f0c2939c1d69fcfc9d
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-05 06:23:11 +0000

    Javadocs

--- trunk/dna/api/src/java/org/jcontainer/dna/Configuration.java
+++ trunk/dna/api/src/java/org/jcontainer/dna/Configuration.java
@@ -8,74 +8,296 @@
 package org.jcontainer.dna;
 
 /**
+ * The configuration object represents hierarchial configuration
+ * data. The data represented by this object is a simplified XML
+ * format. Configuration objects are unable to represent namespace
+ * information and elements can not have mixed content. ie
+ * Configuration elements can not have both a value and child
+ * elements.
  *
- * @version $Revision: 1.2 $ $Date: 2003-09-02 03:47:34 $
+ * @version $Revision: 1.3 $ $Date: 2003-09-05 06:23:11 $
  */
 public interface Configuration
 {
-    String getName();
+   /**
+    * Return the name of the configuration element.
+    *
+    * @return the name of the configuration element.
+    */
+   String getName();
 
-    String getPath();
+   /**
+    * Return the path to the configuration element.
+    * The path should be in the xpath form but may
+    * be the empty string if unabel to determine path.
+    *
+    * @return the path to the configuration element.
+    */
+   String getPath();
 
-    String getLocation();
+   /**
+    * Return the location of configuration element.
+    * Usually of the form "uri[:line number[:column number]]"
+    * if possible. ie "file:myFile.xml:80:2". However the line
+    * number and column number may be elided if unavailable.
+    *
+    * @return the location of configuration element.
+    */
+   String getLocation();
 
-    Configuration[] getChildren();
+   /**
+    * Return an array of all the child elements.
+    *
+    * @return an array of all the child elements.
+    */
+   Configuration[] getChildren();
 
-    Configuration[] getChildren( String name );
+   /**
+    * Return an array of all the child elements with specified name.
+    *
+    * @return an array of all the child elements with specified name.
+    */
+   Configuration[] getChildren( String name );
 
-    Configuration getChild( String name );
+   /**
+    * Return a child Configuration element with specified name.
+    * If no such element exists an element will be autocreated.
+    *
+    * @return a child Configuration element with specified name.
+    */
+   Configuration getChild( String name );
 
-    Configuration getChild( String name, boolean createChild );
+   /**
+    * Return a child Configuration element with specified name.
+    * If no such element exists and createChild is true then an
+    * element will be autocreated otherwise null will be returned.
+    *
+    * @return a child Configuration element with specified name.
+    */
+   Configuration getChild( String name, boolean createChild );
 
-    String getValue()
-        throws ConfigurationException;
+   /**
+    * Return text value of element.
+    *
+    * @return the value
+    * @throws ConfigurationException if no value in element
+    */
+   String getValue()
+      throws ConfigurationException;
 
-    String getValue( String defaultValue );
+   /**
+    * Return text value of element.
+    * Use specified default if no value in element.
+    *
+    * @param defaultValue the default value
+    * @return the value
+    */
+   String getValue( String defaultValue );
 
-    int getValueAsInteger( int defaultValue );
+   /**
+    * Return text value of element as an integer.
+    *
+    * @return the value
+    * @throws ConfigurationException if no value in element
+    *         or value can not be converted to correct type
+    */
+   int getValueAsInteger()
+      throws ConfigurationException;
 
-    int getValueAsInteger()
-        throws ConfigurationException;
+   /**
+    * Return text value of element as an integer.
+    * Use specified default if no value in element or
+    * value can not be converted to correct type.
+    *
+    * @param defaultValue the default value
+    * @return the value
+    */
+   int getValueAsInteger( int defaultValue );
 
-    long getValueAsLong( long defaultValue );
+   /**
+    * Return text value of element as a long.
+    *
+    * @return the value
+    * @throws ConfigurationException if no value in element
+    *         or value can not be converted to correct type
+    */
+   long getValueAsLong()
+      throws ConfigurationException;
 
-    long getValueAsLong()
-        throws ConfigurationException;
+   /**
+    * Return text value of element as a long.
+    * Use specified default if no value in element or
+    * value can not be converted to correct type.
+    *
+    * @param defaultValue the default value
+    * @return the value
+    */
+   long getValueAsLong( long defaultValue );
 
-    boolean getValueAsBoolean( boolean defaultValue );
+   /**
+    * Return text value of element as a boolean.
+    *
+    * @return the value
+    * @throws ConfigurationException if no value in element
+    *         or value can not be converted to correct type
+    */
+   boolean getValueAsBoolean()
+      throws ConfigurationException;
 
-    boolean getValueAsBoolean()
-        throws ConfigurationException;
+   /**
+    * Return text value of element as a boolean.
+    * Use specified default if no value in element or
+    * value can not be converted to correct type.
+    *
+    * @param defaultValue the default value
+    * @return the value
+    */
+   boolean getValueAsBoolean( boolean defaultValue );
 
-    float getValueAsFloat( float defaultValue );
+   /**
+    * Return text value of element as a float.
+    *
+    * @return the value
+    * @throws ConfigurationException if no value in element
+    *         or value can not be converted to correct type
+    */
+   float getValueAsFloat()
+      throws ConfigurationException;
 
-    float getValueAsFloat()
-        throws ConfigurationException;
+   /**
+    * Return text value of element as a float.
+    * Use specified default if no value in element or
+    * value can not be converted to correct type.
+    *
+    * @param defaultValue the default value
+    * @return the value
+    */
+   float getValueAsFloat( float defaultValue );
 
-    String[] getAttributeNames();
+   /**
+    * Return an array of all the attribute names.
+    *
+    * @return an array of all the attribute names.
+    */
+   String[] getAttributeNames();
 
-    String getAttribute( String name )
-        throws ConfigurationException;
+   /**
+    * Return attribute value with specified name.
+    *
+    * @param name the attribute name
+    * @return the attribute value
+    * @throws ConfigurationException if no attribute with
+    *         specified name
+    */
+   String getAttribute( String name )
+      throws ConfigurationException;
 
-    String getAttribute( String name, String defaultValue );
+   /**
+    * Return attribute value with specified name.
+    * If no attribute with specified name then return
+    * default value.
+    *
+    * @param name the attribute name
+    * @param defaultValue the default value
+    * @return the attribute value
+    */
+   String getAttribute( String name, String defaultValue );
 
-    int getAttributeAsInteger( String name, int defaultValue );
+   /**
+    * Return attribute value with specified name as an integer.
+    *
+    * @param name the attribute name
+    * @return the attribute value
+    * @throws ConfigurationException if no attribute with
+    *         specified name or attribute can not be converted
+    *         to correct type
+    */
+   int getAttributeAsInteger( String name )
+      throws ConfigurationException;
 
-    int getAttributeAsInteger( String name )
-        throws ConfigurationException;
+   /**
+    * Return attribute value with specified name as an integer.
+    * If no attribute with specified name or attribute can
+    * not be converted to correct type then return
+    * default value.
+    *
+    * @param name the attribute name
+    * @param defaultValue the default value
+    * @return the attribute value
+    */
+   int getAttributeAsInteger( String name, int defaultValue );
 
-    long getAttributeAsLong( String name, long defaultValue );
 
-    long getAttributeAsLong( String name )
-        throws ConfigurationException;
+   /**
+    * Return attribute value with specified name as a long.
+    *
+    * @param name the attribute name
+    * @return the attribute value
+    * @throws ConfigurationException if no attribute with
+    *         specified name or attribute can not be converted
+    *         to correct type
+    */
+   long getAttributeAsLong( String name )
+      throws ConfigurationException;
 
-    boolean getAttributeAsBoolean( String name, boolean defaultValue );
+   /**
+    * Return attribute value with specified name as a long.
+    * If no attribute with specified name or attribute can
+    * not be converted to correct type then return
+    * default value.
+    *
+    * @param name the attribute name
+    * @param defaultValue the default value
+    * @return the attribute value
+    */
+   long getAttributeAsLong( String name, long defaultValue );
 
-    boolean getAttributeAsBoolean( String name )
-        throws ConfigurationException;
 
-    float getAttributeAsFloat( String name, float defaultValue );
+   /**
+    * Return attribute value with specified name as a boolean.
+    *
+    * @param name the attribute name
+    * @return the attribute value
+    * @throws ConfigurationException if no attribute with
+    *         specified name or attribute can not be converted
+    *         to correct type
+    */
+   boolean getAttributeAsBoolean( String name )
+      throws ConfigurationException;
 
-    float getAttributeAsFloat( String name )
-        throws ConfigurationException;
+   /**
+    * Return attribute value with specified name as a boolean.
+    * If no attribute with specified name or attribute can
+    * not be converted to correct type then return
+    * default value.
+    *
+    * @param name the attribute name
+    * @param defaultValue the default value
+    * @return the attribute value
+    */
+   boolean getAttributeAsBoolean( String name, boolean defaultValue );
+
+   /**
+    * Return attribute value with specified name as afloat.
+    *
+    * @param name the attribute name
+    * @return the attribute value
+    * @throws ConfigurationException if no attribute with
+    *         specified name or attribute can not be converted
+    *         to correct type
+    */
+   float getAttributeAsFloat( String name )
+      throws ConfigurationException;
+
+   /**
+    * Return attribute value with specified name as a float.
+    * If no attribute with specified name or attribute can
+    * not be converted to correct type then return
+    * default value.
+    *
+    * @param name the attribute name
+    * @param defaultValue the default value
+    * @return the attribute value
+    */
+   float getAttributeAsFloat( String name, float defaultValue );
 }
