39cbf25c67bc064f7ca3f31572e73d038afa8db1
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-08 01:45:06 +0000

    Javadocs

--- trunk/dna/impl/src/java/org/jcontainer/dna/impl/ContainerUtil.java
+++ trunk/dna/impl/src/java/org/jcontainer/dna/impl/ContainerUtil.java
@@ -22,11 +22,23 @@ import org.jcontainer.dna.Parameters;
 import org.jcontainer.dna.ResourceLocator;
 
 /**
+ * Utility class to make it easier to process a object
+ * through its lifecycle stages.
  *
- * @version $Revision: 1.1 $ $Date: 2003-07-27 10:44:19 $
+ * @version $Revision: 1.2 $ $Date: 2003-09-08 01:45:06 $
  */
 public class ContainerUtil
 {
+    /**
+     * Supply specified object with Logger if it implements the
+     * LogEnabled interface.
+     *
+     * @param object the object to process
+     * @param logger the logger. If null then the specified object must
+     *        not implement LogEnabled.
+     * @throws IllegalArgumentException if the object is LogEnabled and
+     *         Logger is null
+     */
     public static void enableLogging( final Object object,
                                       final Logger logger )
     {
@@ -41,6 +53,18 @@ public class ContainerUtil
         }
     }
 
+    /**
+     * Supply specified object with ResourceLocator if it implements the
+     * Contextualizable interface.
+     *
+     * @param object the object to process
+     * @param locator the ResourceLocator. If null then the specified
+     *        object must not implement Contextualizable.
+     * @throws IllegalArgumentException if the object is Contextualizable
+     *         and locator is null
+     * @throws MissingResourceException if processing lifecycle stage on
+     *         object throws exception
+     */
     public static void contextualize( final Object object,
                                       final ResourceLocator locator )
         throws MissingResourceException
@@ -56,6 +80,18 @@ public class ContainerUtil
         }
     }
 
+    /**
+     * Supply specified object with ResourceLocator if it implements the
+     * Composable interface.
+     *
+     * @param object the object to process
+     * @param locator the ResourceLocator. If null then the specified
+     *        object must not implement Composable.
+     * @throws IllegalArgumentException if the object is Composable
+     *         and locator is null
+     * @throws MissingResourceException if processing lifecycle stage on
+     *         object throws exception
+     */
     public static void compose( final Object object,
                                 final ResourceLocator locator )
         throws MissingResourceException
@@ -71,6 +107,18 @@ public class ContainerUtil
         }
     }
 
+    /**
+     * Supply specified object with Configuration if it implements the
+     * Configurable interface.
+     *
+     * @param object the object to process
+     * @param configuration the Configuration. If null then the specified
+     *        object must not implement Configurable.
+     * @throws IllegalArgumentException if the object is Configurable
+     *         and configuration is null
+     * @throws ConfigurationException if processing lifecycle stage on
+     *         object throws exception
+     */
     public static void configure( final Object object,
                                   final Configuration configuration )
         throws ConfigurationException
@@ -86,6 +134,18 @@ public class ContainerUtil
         }
     }
 
+    /**
+     * Supply specified object with Parameters if it implements the
+     * Parameterizable interface.
+     *
+     * @param object the object to process
+     * @param parameters the Parameters. If null then the specified
+     *        object must not implement Parameterizable.
+     * @throws IllegalArgumentException if the object is Parameterizable
+     *         and parameters is null
+     * @throws ParameterException if processing lifecycle stage on
+     *         object throws exception
+     */
     public static void parameterize( final Object object,
                                      final Parameters parameters )
         throws ParameterException
@@ -101,6 +161,14 @@ public class ContainerUtil
         }
     }
 
+    /**
+     * Initialize specified object if it implements the
+     * Active interface.
+     *
+     * @param object the object to process
+     * @throws Exception if processing lifecycle stage on
+     *         object throws exception
+     */
     public static void initialize( final Object object )
         throws Exception
     {
@@ -110,6 +178,14 @@ public class ContainerUtil
         }
     }
 
+    /**
+     * Dispose specified object if it implements the
+     * Active interface.
+     *
+     * @param object the object to process
+     * @throws Exception if processing lifecycle stage on
+     *         object throws exception
+     */
     public static void dispose( final Object object )
         throws Exception
     {
