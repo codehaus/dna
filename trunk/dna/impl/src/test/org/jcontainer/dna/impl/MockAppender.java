523902f730b1610a4ca7f0b0f0129aedf176f025
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-09 03:59:19 +0000

    Add in mock appender

---
+++ trunk/dna/impl/src/test/org/jcontainer/dna/impl/MockAppender.java
@@ -0,0 +1,74 @@
+package org.jcontainer.dna.impl;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ErrorHandler;
+
+class MockAppender
+   implements Appender
+{
+   boolean m_output;
+   Level m_priority;
+   String m_message;
+   Throwable m_throwable;
+
+   public void doAppend( LoggingEvent event )
+   {
+      m_output = true;
+      m_priority = event.getLevel();
+      m_message = (String) event.getMessage();
+      m_throwable = event.getThrowableInformation().getThrowable();
+   }
+
+   public void addFilter( Filter filter )
+   {
+   }
+
+   public Filter getFilter()
+   {
+      return null;
+   }
+
+   public void clearFilters()
+   {
+   }
+
+   public void close()
+   {
+   }
+
+   public String getName()
+   {
+      return null;
+   }
+
+   public void setErrorHandler( ErrorHandler errorHandler )
+   {
+   }
+
+   public ErrorHandler getErrorHandler()
+   {
+      return null;
+   }
+
+   public void setLayout( Layout layout )
+   {
+   }
+
+   public Layout getLayout()
+   {
+      return null;
+   }
+
+   public void setName( String classname )
+   {
+   }
+
+   public boolean requiresLayout()
+   {
+      return false;
+   }
+}
