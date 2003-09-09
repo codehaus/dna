4b32c19db6eaf225ec72dce63e84eb0ad5f7b8f7
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-09 02:13:15 +0000

    Add in mock log target to help with unit testing

---
+++ dna/impl/src/test/org/jcontainer/dna/impl/MockLogTarget.java
@@ -0,0 +1,20 @@
+package org.jcontainer.dna.impl;
+
+import org.apache.log.LogTarget;
+import org.apache.log.Priority;
+import org.apache.log.LogEvent;
+
+class MockLogTarget
+   implements LogTarget
+{
+   Priority m_priority;
+   String m_message;
+   Throwable m_throwable;
+
+   public void processEvent( final LogEvent event )
+   {
+      m_priority = event.getPriority();
+      m_message = event.getMessage();
+      m_throwable = event.getThrowable();
+   }
+}
