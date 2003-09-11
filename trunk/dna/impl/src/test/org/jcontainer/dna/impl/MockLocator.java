2a4a9311880b7aeb48af80561c12965d1f2bd496
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-11 06:11:20 +0000

    Add in mock locator to enable testing

---
+++ trunk/dna/impl/src/test/org/jcontainer/dna/impl/MockLocator.java
@@ -0,0 +1,49 @@
+package org.jcontainer.dna.impl;
+
+import org.xml.sax.Locator;
+
+class MockLocator
+   implements Locator
+{
+   private final String _publicId;
+   private final String _systemId;
+   private int _lineNumber = -1;
+   private int _columnNumber = -1;
+
+   public MockLocator( final String publicId,
+                       final String systemId )
+   {
+      _publicId = publicId;
+      _systemId = systemId;
+   }
+
+   public String getPublicId()
+   {
+      return _publicId;
+   }
+
+   public String getSystemId()
+   {
+      return _systemId;
+   }
+
+   public int getLineNumber()
+   {
+      return _lineNumber;
+   }
+
+   public void setLineNumber( int lineNumber )
+   {
+      _lineNumber = lineNumber;
+   }
+
+   public int getColumnNumber()
+   {
+      return _columnNumber;
+   }
+
+   public void setColumnNumber( int columnNumber )
+   {
+      _columnNumber = columnNumber;
+   }
+}
