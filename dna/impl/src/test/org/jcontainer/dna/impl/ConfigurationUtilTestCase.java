3183f69621f9592cfa26d5c96945b793a70c5ced
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-11 05:42:49 +0000

    Complete coverage of unit tests

--- dna/impl/src/test/org/jcontainer/dna/impl/ConfigurationUtilTestCase.java
+++ dna/impl/src/test/org/jcontainer/dna/impl/ConfigurationUtilTestCase.java
@@ -1,14 +1,29 @@
 package org.jcontainer.dna.impl;
 
+import java.lang.reflect.Proxy;
+import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.sax.SAXResult;
+
 import junit.framework.TestCase;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.Document;
+import org.w3c.dom.Text;
+import org.w3c.dom.Comment;
+import org.jcontainer.dna.Configuration;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
 
 public class ConfigurationUtilTestCase
    extends TestCase
 {
+   private static final String DOC_FACTORY = "javax.xml.parsers.DocumentBuilderFactory";
+
    public void testToElementWithBasicConfiguration()
       throws Exception
    {
@@ -23,7 +38,6 @@ public class ConfigurationUtilTestCase
       final NamedNodeMap attributes = element.getAttributes();
       assertEquals( "attributes.getLength()", 0, attributes.getLength() );
    }
-
 
    public void testToElementWithConfigurationWithValue()
       throws Exception
@@ -97,7 +111,7 @@ public class ConfigurationUtilTestCase
       final String name = "meep";
       final DefaultConfiguration configuration = new DefaultConfiguration( name, "", "" );
 
-      final String property = System.getProperty( "javax.xml.parsers.DocumentBuilderFactory" );
+      final String property = System.getProperty( DOC_FACTORY );
       try
       {
          System.setProperty( "javax.xml.parsers.DocumentBuilderFactory",
@@ -110,10 +124,209 @@ public class ConfigurationUtilTestCase
       }
       finally
       {
-         System.setProperty( "javax.xml.parsers.DocumentBuilderFactory",
-                             property );
+         if ( null != property )
+         {
+            System.setProperty( DOC_FACTORY, property );
+         }
+         else
+         {
+            System.getProperties().remove( DOC_FACTORY );
+         }
       }
       fail( "Expected to fail to create element as " +
             "invlaid document factory property" );
+   }
+
+   public void testToConfigurationFromBasicElement()
+      throws Exception
+   {
+      final Document document = createDocument();
+      final String name = "meep";
+      final Element element = document.createElement( name );
+      final Configuration configuration = ConfigurationUtil.toConfiguration( element );
+
+      assertEquals( "configuration.getName()", name, configuration.getName() );
+      assertEquals( "configuration.getPath()",
+                    ConfigurationUtil.ROOT_PATH,
+                    configuration.getPath() );
+      assertEquals( "configuration.getLocation()",
+                    "dom-gen",
+                    configuration.getLocation() );
+   }
+
+   public void testToConfigurationFromElementWithValue()
+      throws Exception
+   {
+      final Document document = createDocument();
+      final String name = "meep";
+      final String value = "text";
+      final Element element = document.createElement( name );
+      final Text text = document.createTextNode( value );
+      element.appendChild( text );
+      final Configuration configuration = ConfigurationUtil.toConfiguration( element );
+
+      assertEquals( "configuration.getName()", name, configuration.getName() );
+      assertEquals( "configuration.getPath()",
+                    ConfigurationUtil.ROOT_PATH,
+                    configuration.getPath() );
+      assertEquals( "configuration.getLocation()",
+                    "dom-gen",
+                    configuration.getLocation() );
+      assertEquals( "configuration.getValue()",
+                    value,
+                    configuration.getValue() );
+   }
+
+   public void testToConfigurationFromElementWithMultipleValueFragments()
+      throws Exception
+   {
+      final Document document = createDocument();
+      final String name = "meep";
+      final String value = "text";
+      final Element element = document.createElement( name );
+      final Text text = document.createTextNode( value );
+      element.appendChild( text );
+      final Text text2 = document.createTextNode( value );
+      element.appendChild( text2 );
+      final Configuration configuration = ConfigurationUtil.toConfiguration( element );
+
+      assertEquals( "configuration.getName()", name, configuration.getName() );
+      assertEquals( "configuration.getPath()",
+                    ConfigurationUtil.ROOT_PATH,
+                    configuration.getPath() );
+      assertEquals( "configuration.getLocation()",
+                    "dom-gen",
+                    configuration.getLocation() );
+      assertEquals( "configuration.getValue()",
+                    value + value,
+                    configuration.getValue() );
+   }
+
+   public void testToConfigurationFromElementWithInternalComment()
+      throws Exception
+   {
+      final Document document = createDocument();
+      final String name = "meep";
+      final Element element = document.createElement( name );
+      final Comment comment = document.createComment( "comment" );
+      element.appendChild( comment );
+      final Configuration configuration = ConfigurationUtil.toConfiguration( element );
+
+      assertEquals( "configuration.getName()", name, configuration.getName() );
+      assertEquals( "configuration.getPath()",
+                    ConfigurationUtil.ROOT_PATH,
+                    configuration.getPath() );
+      assertEquals( "configuration.getLocation()",
+                    "dom-gen",
+                    configuration.getLocation() );
+      assertEquals( "configuration.getValue()",
+                    null,
+                    configuration.getValue( null ) );
+   }
+
+   public void testToConfigurationFromElementWithAttributes()
+      throws Exception
+   {
+      final Document document = createDocument();
+      final String name = "meep";
+      final String key = "key";
+      final String value = "value";
+      final Element element = document.createElement( name );
+      element.setAttribute( key, value );
+      final Configuration configuration = ConfigurationUtil.toConfiguration( element );
+
+      assertEquals( "configuration.getName()", name, configuration.getName() );
+      assertEquals( "configuration.getPath()",
+                    ConfigurationUtil.ROOT_PATH,
+                    configuration.getPath() );
+      assertEquals( "configuration.getLocation()",
+                    "dom-gen",
+                    configuration.getLocation() );
+      assertEquals( "configuration.getAttributeNames().length",
+                    1,
+                    configuration.getAttributeNames().length );
+      assertEquals( "configuration.getAttribute( key )",
+                    value,
+                    configuration.getAttribute( key ) );
+   }
+
+   public void testToConfigurationFromElementWithChildren()
+      throws Exception
+   {
+      final Document document = createDocument();
+      final String name = "meep";
+      final String childName = "lilmeep";
+      final Element element = document.createElement( name );
+      final Element childElement = document.createElement( childName );
+      element.appendChild( childElement );
+
+      final Configuration configuration = ConfigurationUtil.toConfiguration( element );
+
+      assertEquals( "configuration.getName()", name, configuration.getName() );
+      assertEquals( "configuration.getPath()",
+                    ConfigurationUtil.ROOT_PATH,
+                    configuration.getPath() );
+      assertEquals( "configuration.getLocation()",
+                    "dom-gen",
+                    configuration.getLocation() );
+      assertEquals( "configuration.getAttributeNames().length",
+                    0,
+                    configuration.getAttributeNames().length );
+      assertEquals( "configuration.getChildren().length",
+                    1,
+                    configuration.getChildren().length );
+      final Configuration child = configuration.getChildren()[ 0 ];
+      assertEquals( "child.name", childName, child.getName() );
+      assertEquals( "child.getPath()", "meep", child.getPath() );
+   }
+
+   public void testSerializeToResult()
+      throws Exception
+   {
+      final String name = "meep";
+      final DefaultConfiguration configuration = new DefaultConfiguration( name, "", "" );
+
+      final MockInvocationRecorder recorder = new MockInvocationRecorder();
+      recorder.addInvocation( SAXMethods.START_DOCUMENT,
+                              new Object[ 0 ],
+                              null );
+      recorder.addInvocation( SAXMethods.START_ELEMENT,
+                              new Object[]{"", name, name,
+                                           MockSAXConfigurationSerializer.ATTRIBUTES},
+                              null );
+      recorder.addInvocation( SAXMethods.END_ELEMENT,
+                              new Object[]{"", name, name},
+                              null );
+      recorder.addInvocation( SAXMethods.END_DOCUMENT,
+                              new Object[ 0 ],
+                              null );
+
+      final ContentHandler handler = (ContentHandler)
+         Proxy.newProxyInstance( getClass().getClassLoader(),
+                                 new Class[]{ContentHandler.class},
+                                 recorder );
+
+      final SAXResult result = new SAXResult( handler );
+
+      ConfigurationUtil.serializeToResult( result, configuration );
+   }
+
+   public void testBuildFromXML()
+      throws Exception
+   {
+      final String data = "<element/>";
+      final InputSource input = new InputSource();
+      input.setCharacterStream( new StringReader( data ) );
+      final Configuration configuration = ConfigurationUtil.buildFromXML( input );
+      assertEquals( "configuration.name", "element", configuration.getName() );
+      assertEquals( "configuration.path", "", configuration.getPath() );
+      assertEquals( "configuration.location", "", configuration.getLocation() );
+   }
+
+   private Document createDocument() throws ParserConfigurationException
+   {
+      final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      final DocumentBuilder builder = factory.newDocumentBuilder();
+      return builder.newDocument();
    }
 }
