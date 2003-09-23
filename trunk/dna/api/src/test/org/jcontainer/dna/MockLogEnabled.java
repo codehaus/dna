ae0e793521592b4bf17c5849900bf8b9b8b4b41b
pdonald <pdonald@5ab15c8b-ab72-0410-830a-dc9a8d3a8d79>
2003-09-23 02:35:35 +0000

    Add mock class to enable unit testing

---
+++ trunk/dna/api/src/test/org/jcontainer/dna/MockLogEnabled.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.dna;
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-09-23 02:33:39 $
+ */
+class MockLogEnabled
+    extends AbstractLogEnabled
+{
+}
